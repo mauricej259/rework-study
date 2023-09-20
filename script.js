c8ed198911b468c25b7673a3c048ebcf4c258c40
Mona Lisa <mauricejohnson@Maurices-MacBook-Air.local>
2023-09-19 21:28:21 -0400

    index.html

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var topics= "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x =0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+/*
+if (topic === 'HTML') {
+    console.log("let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("let's study Git");
+} else if (topic === 'JavaScript') {
+    HTMLFormControlsCollection.log("let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
