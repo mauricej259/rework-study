50c105597c3d28dd7c77aa777d24ff7c13c38c89
Mona Lisa <mauricejohnson@Maurices-MacBook-Air.local>
2023-09-21 19:41:49 -0400

    assets/script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random()* topics.length)];
+
 function listTopics() {
   for (var x =0; x < topics.length; x++) {
     console.log(topics[x]);
 }
 }
+
 function select topic() {
 if (topic === 'HTML') {
     console.log("let's study HTML!");
@@ -17,3 +20,8 @@ if (topic === 'HTML') {
     console.log('Please try again!');
 }
 }
+
+console.log('here are the topics we learned through Prework');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
